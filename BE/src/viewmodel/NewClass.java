e69629385775bb3f18839debe07abb3e25f1823b
dangnb <dangnb163@gmail.com>
2024-09-29 19:04:07 +0700

    add

---
+++ BE/src/viewmodel/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package viewmodel;
+
+/**
+ *
+ * @author Ddawng
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i + ". Yêu chú Du");
+        }
+    }
+    
+}
